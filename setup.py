aa0e261cfefb39bac6a951359348147a4e8202a3
Jahnavi Maddhuri <jahnavi.maddhuri@gmail.com>
2024-10-23 21:21:34 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="databricks_etl",
+    version="0.0.1",
+    description="ETL Query CLI",
+    author="Jahnavi Maddhuri",
+    author_email="jahnavi.maddhuri@duke.edu",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "python-dotenv",
+        "databricks-sql-connector",
+        "rust",
+    ],
+    entry_points={
+        "console_scripts": [
+            "etl_query=main:",
+        ],
+    },
+)
